71aedf7240fe10016ee6c3ac4110ef6b88c78df2
Roman Kamenivskyi <jumji19992412@gmail.com>
2020-10-16 20:47:14 +0300

    Updates

---
+++ precache-manifest.1593952a41e8fbd2b143887a36ea4fbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd66ef64b869d810af605ea40efc6385",
+    "url": "/cocktailsApp/index.html"
+  },
+  {
+    "revision": "d4948d4da192874ebb8e",
+    "url": "/cocktailsApp/static/css/main.d2e3d545.chunk.css"
+  },
+  {
+    "revision": "c025243f0a968fcf85a3",
+    "url": "/cocktailsApp/static/js/2.6386df88.chunk.js"
+  },
+  {
+    "revision": "d4948d4da192874ebb8e",
+    "url": "/cocktailsApp/static/js/main.ae574ced.chunk.js"
+  },
+  {
+    "revision": "590401ca8fe1b63a9871",
+    "url": "/cocktailsApp/static/js/runtime-main.17654650.js"
+  }
+]);
